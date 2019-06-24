611eadd76979c8aa33917e212e3d6fbb8fcc0a3c
cheerss <zjdxwwx@163.com>
2019-06-24 12:50:04 +0000

    train_finetune.sh -> prune.sh

--- prune.sh
+++ prune.sh
@@ -2,7 +2,7 @@
 prune_rate=0.67
 train_dir="vgg-cifar10-model/n1"
 mkdir -p $train_dir/$prune_rate
-CUDA_VISIBLE_DEVICES=1 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar10" --data_dir="./data" 
+CUDA_VISIBLE_DEVICES=1 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar10" --data_dir="./data" \
     --network="vgg16" --alpha=1.0 --beta=0.0 --gamma=3.0 --prune_rate=$prune_rate \
     2>&1 | tee -a $train_dir/$prune_rate/finetune.log
 
@@ -27,32 +27,37 @@ prune_rate=0.01
 train_dir="mobilenet-cifar10-model/n1"
 mkdir -p $train_dir/$prune_rate
 CUDA_VISIBLE_DEVICES=1 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar10" --data_dir="./data" \
-    --network="mobilenet_for_cifar" 2>&1 | tee -a $train_dir/$prune_rate/finetune.log
+    --network="mobilenet_for_cifar" --alpha=1.0 --beta=1.0 --gamma=1.0 --prune_rate=$prune_rate \
+    2>&1 | tee -a $train_dir/$prune_rate/finetune.log
 
 ## mobilenet-cifar100
 prune_rate=0.01
 train_dir="mobilenet-cifar100-model/n1"
 mkdir -p $train_dir/$prune_rate
 CUDA_VISIBLE_DEVICES=1 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar100" --data_dir="./data" \
-    --network="mobilenet_for_cifar" 2>&1 | tee -a $train_dir/$prune_rate/finetune.log
+    --network="mobilenet_for_cifar" --alpha=1.0 --beta=1.0 --gamma=1.0 --prune_rate=$prune_rate \
+    2>&1 | tee -a $train_dir/$prune_rate/finetune.log
 
 ## resnet32-cifar10
 prune_rate=0.01
 train_dir="resnet32-cifar10-model/n1"
 mkdir -p $train_dir/$prune_rate
 CUDA_VISIBLE_DEVICES=3 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar10" --data_dir="./data" \
-    --network="resnet32" 2>&1 | tee -a $train_dir/$prune_rate/finetune.log
+    --network="resnet32" --alpha=1.0 --beta=1.0 --gamma=1.0 --prune_rate=$prune_rate \
+    2>&1 | tee -a $train_dir/$prune_rate/finetune.log
 
 ## resnet32-cifar100
 prune_rate=0.01
 train_dir="resnet32-cifar100-model/n1"
 mkdir -p $train_dir/$prune_rate
 CUDA_VISIBLE_DEVICES=3 python -u auto_prune.py --train_dir=$train_dir --dataset="cifar100" --data_dir="./data" \
-    --network="resnet32" 2>&1 | tee -a $train_dir/$prune_rate/finetune.log
+    --network="resnet32" --alpha=1.0 --beta=1.0 --gamma=1.0 --prune_rate=$prune_rate \
+    2>&1 | tee -a $train_dir/$prune_rate/finetune.log
 
 ## resnet18-imagenet
-prune_rate=0.001
+prune_rate=0.02
 train_dir="resnet18-imagenet-model/n1"
 mkdir -p $train_dir/$prune_rate
-CUDA_VISIBLE_DEVICES=1 python -u auto_prune.py --train_dir=$train_dir --dataset="imagenet" --data_dir="./data" \
-    --network="resnet18" 2>&1 | tee -a $train_dir/$prune_rate/finetune.log
+CUDA_VISIBLE_DEVICES=1,2 python -u auto_prune.py --train_dir=$train_dir --dataset="imagenet" --data_dir="./data" \
+    --network="resnet18" --alpha=1.0 --beta=1.0 --gamma=1.0 --prune_rate=$prune_rate \
+    2>&1 | tee -a $train_dir/$prune_rate/finetune.log
